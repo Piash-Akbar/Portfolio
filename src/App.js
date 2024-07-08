b606249d9a4392496c653ef63e38e58507449205
Piash-Akbar <sknafisakbar@gmail.com>
2024-07-09 01:49:48 +0600

    iiii

--- src/App.js
+++ src/App.js
@@ -12,16 +12,42 @@ import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
-    <HashRouter>
+    // <HashRouter>
+    //   <Navbar />
+    //     <Routes>
+    //       <Route exact path='/' element={<Home />}></Route>
+    //       <Route path='/Career' element={<Career />}> </Route>
+    //       <Route path='/Academic' element={<Academic />}></Route>
+    //       <Route path='/Personal' element={<Personal />}></Route>
+    //       <Route path='/Contact' element={<Contact />}></Route>
+    //     </Routes>
+    // </HashRouter>
+
+    <>
       <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />}></Route>
-          <Route path='/Career' element={<Career />}> </Route>
-          <Route path='/Academic' element={<Academic />}></Route>
-          <Route path='/Personal' element={<Personal />}></Route>
-          <Route path='/Contact' element={<Contact />}></Route>
-        </Routes>
-    </HashRouter>
+      <switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route exact path='/Career'>
+          <Career />
+        </Route>
+
+        <Route exact path='/Academic'>
+          <Academic />
+        </Route>
+
+        <Route exact path='/Personal'>
+          <Personal />
+        </Route>
+
+        <Route exact path='/Contact'>
+          <Contact />
+        </Route>
+
+      </switch>
+    </>
     
 
 )}
