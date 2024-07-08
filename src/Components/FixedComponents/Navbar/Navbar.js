144228ddeff145e70f976aa8b3045fac4a259074
Piash-Akbar <sknafisakbar@gmail.com>
2024-07-09 02:59:57 +0600

    kkk

--- src/Components/FixedComponents/Navbar/Navbar.js
+++ src/Components/FixedComponents/Navbar/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './Navbar.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import {faFacebook} from '@fortawesome/free-brands-svg-icons';
 import Propic from '../../../Images/Pro.png' 
 
 
