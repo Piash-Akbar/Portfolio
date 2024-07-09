b9c8cc1b09c2d703842b52d8c71565a41a4863a8
Piash-Akbar <sknafisakbar@gmail.com>
2024-07-10 00:01:13 +0600

    added cv

--- src/Components/FixedComponents/HeadCard/HeadCard.js
+++ src/Components/FixedComponents/HeadCard/HeadCard.js
@@ -12,7 +12,7 @@ const HeadCard  = (props) => {
             <p>{props.works}</p>
             </div>
           </div>
-          <div className='CV'><a href="https://www.linkedin.com/in/sk-nafis-akbar-038899155/" target='_blank' rel="noreferrer">cv</a></div>
+          <div className='CV'><a href="https://drive.google.com/file/d/1msy2-ruz9oqnpSEJUCHSn789EUmJ2LbM/view?usp=sharing" target='_blank' rel="noreferrer">cv</a></div>
         </div>
       </div>
     </div>
