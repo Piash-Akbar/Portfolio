adae1090860bfae9a2e11ef987319a727fe068eb
Piash-Akbar <sknafisakbar@gmail.com>
2025-04-29 22:15:33 +0600

    mended

--- src/Components/FixedComponents/Navbar/Navbar.js
+++ src/Components/FixedComponents/Navbar/Navbar.js
@@ -4,7 +4,6 @@ import Propic from '../../../Images/Pro.png'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBars,faTimes} from '@fortawesome/free-solid-svg-icons';
 import { useRef } from 'react';
-import { Link } from 'react-router-dom'
 
 
 const Navbar = () => {
@@ -29,18 +28,15 @@ const Navbar = () => {
         </div>
 
         {/* Nav Right  */}
-        <nav className='nav-right' ref={navRef}>
-          
+        <nav className='nav-right' ref={navRef}>       
         <ul className='lists'>
-          <li>
-            <Link to='/Career'>Car</Link>
-          </li>
           <li className='list-item'><a href="/" className="home"  >Home</a></li>
           <li className='list-item'><a href="/Career" className="about">Expertise</a></li>
           <li className='list-item'><a href="/Academic" className="about">Academic</a></li>
           <li className='list-item'><a href="/Personal" className="about">Personal</a></li>
           <li className='list-item'><a href="/Contact" className="about">Contact</a></li>
         </ul>
+
 
         <button className='nav-btn nav-close-btn' onClick={showNavBar}>
           <FontAwesomeIcon className='fa' icon={faTimes} />
